d7f4c1481bbc1e6290b48f8a11e26ea87716e16d
Администратор <a2driano@gmail.com>
2017-02-08 21:51:15 +0200

    [fix] - Delete item

--- app/src/main/java/com/a2driano/note100/activities/NoteListActivity.java
+++ app/src/main/java/com/a2driano/note100/activities/NoteListActivity.java
@@ -2,6 +2,7 @@ package com.a2driano.note100.activities;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Parcelable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,7 +16,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.a2driano.note100.R;
@@ -23,25 +27,30 @@ import com.a2driano.note100.data.NoteDbSchema;
 import com.a2driano.note100.data.NoteStore;
 import com.a2driano.note100.model.NoteModel;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import static com.a2driano.note100.util.UtilNote.getReadableModifiedDate;
 
 public class NoteListActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE_UUID = "com.a2driano.note100.activities.UUID";
-//    public final static String EXTRA_MESSAGE_COLOR = "com.a2driano.note100.activities.COLOR";
-
+    //    public final static String EXTRA_MESSAGE_COLOR = "com.a2driano.note100.activities.COLOR";
     private RecyclerView mNoteRecyclerView;
     private NoteAdapter mNoteAdapter;
+    private FloatingActionButton fab;
     private Toolbar mToolbar;
     private List<NoteModel> notes;
     private NoteStore mNoteStore;
     private SharedPreferences mPreferences;
     private String sortingVariable;
     private boolean reverseVariable;
+    private boolean mDeleteAll;
+    private boolean mMenuDeleteAllVisible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +60,7 @@ public class NoteListActivity extends AppCompatActivity {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -65,6 +74,9 @@ public class NoteListActivity extends AppCompatActivity {
         mNoteRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mNoteRecyclerView.setHasFixedSize(true);
         registerForContextMenu(mNoteRecyclerView);
+
+        mDeleteAll = false;
+        mMenuDeleteAllVisible = false;
     }
 
     /**
@@ -101,6 +113,9 @@ public class NoteListActivity extends AppCompatActivity {
         updateUI();
     }
 
+    /**
+     * For refresh activity
+     */
     private void updateUI() {
         mNoteStore = NoteStore.get(NoteListActivity.this);
         notes = mNoteStore.getNotes();
@@ -116,21 +131,21 @@ public class NoteListActivity extends AppCompatActivity {
     }
 
     /**
-     * SORTING LIST
+     * Sorting list
      */
     private void sortNoteList(String parameter, boolean reverse) {
         if (parameter.equals(NoteDbSchema.NoteTable.Cols.TEXT)) {
             Collections.sort(notes, new Comparator<NoteModel>() {
                 @Override
                 public int compare(NoteModel o1, NoteModel o2) {
-                    return o1.getText().compareTo(o2.getText());
+                    return o1.getText().toUpperCase().compareTo(o2.getText().toUpperCase());
                 }
             });
             if (!reverse)
                 Collections.sort(notes, new Comparator<NoteModel>() {
                     @Override
                     public int compare(NoteModel o1, NoteModel o2) {
-                        return o2.getText().compareTo(o1.getText());
+                        return o2.getText().toUpperCase().compareTo(o1.getText().toUpperCase());
                     }
                 });
         } else {
@@ -170,6 +185,15 @@ public class NoteListActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        menu.setGroupVisible(R.id.menu_delete_actionbar, mMenuDeleteAllVisible);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    /**
+     * Action on menu
+     */
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.create_note:
@@ -203,11 +227,50 @@ public class NoteListActivity extends AppCompatActivity {
             case R.id.menu_settings:
                 break;
             case R.id.menu_delete:
+                mDeleteAll = true;
+                updateUI();
+                mMenuDeleteAllVisible = true;
+                invalidateOptionsMenu();
+                fab.setVisibility(View.GONE);
+                break;
+            case R.id.menu_delete_all:
+                HashMap<Integer, UUID> hashMapForDelete = mNoteAdapter.mNoteHolderListForDelete;
+                for (Map.Entry<Integer, UUID> entry : hashMapForDelete.entrySet()) {
+                    int position = entry.getKey();
+                    UUID uuid = entry.getValue();
+
+                    NoteModel noteModel = mNoteStore.getNote(uuid);
+                    mNoteStore.deleteNote(noteModel);
+//                    mNoteAdapter.mNoteModelList.remove(position);
+//                    mNoteAdapter.notifyItemRemoved(position);
+                }
+//                updateUI();
+                onResume();
+                fab.setVisibility(View.VISIBLE);
+                hideMenuActionBar();
+                break;
+            /** Cancel delete menu */
+            case R.id.menu_delete_cancel:
+                hideMenuActionBar();
+                mNoteAdapter.notifyDataSetChanged();
                 break;
             default:
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void hideMenuActionBar() {
+        mDeleteAll = false;
+        mMenuDeleteAllVisible = false;
+        invalidateOptionsMenu();
+
+        /** Remember top position on screen
+         * and after setAdapter
+         * return to necessary position*/
+        Parcelable parcelable = mNoteRecyclerView.getLayoutManager().onSaveInstanceState();
+        mNoteRecyclerView.setAdapter(mNoteAdapter);
+        mNoteRecyclerView.getLayoutManager().onRestoreInstanceState(parcelable);
     }
 
     @Override
@@ -273,6 +336,7 @@ public class NoteListActivity extends AppCompatActivity {
 
         private NoteModel mNoteModel;
         private LinearLayout mLinearLayout;
+        private LinearLayout mLinearLayoutInsideNote;
 
         public NoteHolder(View itemView) {
             super(itemView);
@@ -282,6 +346,7 @@ public class NoteListActivity extends AppCompatActivity {
             mNoteUuid = (TextView) itemView.findViewById(R.id.uuid);
             mNoteColor = (TextView) itemView.findViewById(R.id.color);
             mLinearLayout = (LinearLayout) findViewById(R.id.note_layout);
+            mLinearLayoutInsideNote = (LinearLayout) findViewById(R.id.delete_note_host);
             itemView.setOnClickListener(this);
             itemView.setOnCreateContextMenuListener(this);
             itemView.setLongClickable(true);
@@ -312,10 +377,12 @@ public class NoteListActivity extends AppCompatActivity {
 
     private class NoteAdapter extends RecyclerView.Adapter<NoteHolder> {
         private List<NoteModel> mNoteModelList;
+        private HashMap<Integer, UUID> mNoteHolderListForDelete;
         private int position;
 
         public NoteAdapter(List<NoteModel> noteModelList) {
             mNoteModelList = noteModelList;
+            mNoteHolderListForDelete = new HashMap<>();
         }
 
         public int getPosition() {
@@ -331,12 +398,14 @@ public class NoteListActivity extends AppCompatActivity {
             LayoutInflater layoutInflater = LayoutInflater.from(NoteListActivity.this);
             View view = layoutInflater.inflate(R.layout.list_item_note, parent, false);
 
+//            view.findViewById(R.id.delete_note_host).setVisibility(View.VISIBLE);//test
+
             NoteHolder holder = new NoteHolder(view);
             return holder;
         }
 
         @Override
-        public void onBindViewHolder(final NoteHolder holder, int position) {
+        public void onBindViewHolder(final NoteHolder holder, final int position) {
             holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
                 @Override
                 public boolean onLongClick(View v) {
@@ -350,6 +419,29 @@ public class NoteListActivity extends AppCompatActivity {
             String color = noteModel.getColor().toUpperCase();
             int colorLayout = getResources().getIdentifier(color, "color", getPackageName());
             holder.itemView.setBackgroundResource(colorLayout);
+
+            /** When user click delete button in menu
+             * in right side draw CheckBox
+             * and save position and UUID of holder
+             * in HashMap */
+            if (mDeleteAll) {
+                TextView view = (TextView) holder.itemView.findViewById(R.id.uuid);
+                String uuidString = view.getText().toString();
+                final UUID uuid = UUID.fromString(uuidString);
+                holder.itemView.findViewById(R.id.delete_note_host).setVisibility(View.VISIBLE);
+
+                CheckBox checkBox = (CheckBox) holder.itemView.findViewById(R.id.checkBox_for_delete);
+                checkBox.setOnCheckedChangeListener(
+                        new CompoundButton.OnCheckedChangeListener() {
+                            @Override
+                            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                                if (isChecked) {
+                                    mNoteHolderListForDelete.put(position, uuid);
+                                }
+                            }
+                        }
+                );
+            }
         }
 
         @Override
@@ -359,6 +451,7 @@ public class NoteListActivity extends AppCompatActivity {
 
         public void setNotes(List<NoteModel> notes) {
             mNoteModelList = notes;
+            mNoteHolderListForDelete = new HashMap<>();
         }
     }
 }
